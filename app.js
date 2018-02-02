36a68f2168c895ad37a94e687a91b98597565f06
Khaleel Younis <khaleelyounis1@gmail.com>
2018-02-02 12:46:46 -0800

    handled google auth

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const port = process.env.PORT || 3000;
+const passport = require('passport');
+
+//Passport Config
+require('./config/passport')(passport);
+
+//Load Routes
+const auth = require('./routes/auth');
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('It works!');
-})
+});
+
+//Use Routes
+app.use('/auth', auth);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('You have entered the realm of development on port: ' + port);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://cal:cal@ds121898.mlab.com:21898/storybooks-dev',
+    googleClientID: '163949585882-ja4rf8j7qepl8icf13ikfuaano0hrsfr.apps.googleusercontent.com',
+    googleClientSecret: 'E2rBkAKwDLUMMhWD35sc2N2X'
+}
