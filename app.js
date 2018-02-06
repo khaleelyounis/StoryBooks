f2b8d19b7ebd74c6215cb0e66bf3fd8581dde9ee
Khaleel Younis <khaleelyounis1@gmail.com>
2018-02-06 12:41:33 -0800

    Finished version 1.0.0alpha

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const port = process.env.PORT || 3000;
 const app = express();
+
+//Load Models
+require('./models/Users');
+require('./models/Story');
 
 //Load Routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories')
 
 //Load Keys file
 const keys = require('./config/keys');
 
-//Load User Model
-require('./models/Users');
+//Handlebars Helpers
+const {
+    truncate,
+    stripTags,
+    formatDate,
+    select,
+    editIcon
+} = require('./helpers/hbs')
 
 //Passport Config
 require('./config/passport')(passport);
@@ -30,8 +43,24 @@ mongoose.connect(keys.mongoURI)
     })
     .catch(err => console.log(err));
 
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+//Method Override Middleware
+app.use(methodOverride('_method'));
+
 //Handlebars Middleware
 app.engine('handlebars', exphbs({
+    helpers: {
+        truncate: truncate,
+        stripTags: stripTags,
+        formatDate: formatDate,
+        select: select,
+        editIcon: editIcon
+    },
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
@@ -53,9 +82,15 @@ app.use((req, res, next) => {
     next();
 })
 
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Use Routes
 app.use('/auth', auth);
 app.use('/', index);
+app.use('/stories', stories);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('You have entered the realm of development on port: ' + port);
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://cal:cal@ds121898.mlab.com:21898/storybooks-dev',
-    googleClientID: '163949585882-ja4rf8j7qepl8icf13ikfuaano0hrsfr.apps.googleusercontent.com',
-    googleClientSecret: 'E2rBkAKwDLUMMhWD35sc2N2X'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
